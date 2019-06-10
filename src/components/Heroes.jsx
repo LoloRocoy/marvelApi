9946a5a3b00e0a14fe8455ecd9309da7c8a23ee5
ManeRomero <algoritmo717@gmail.com>
2019-06-10 16:51:52 +0200

    Project started · First component correctly linked

---
+++ src/components/Heroes.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Heroes extends Component {
+    constructor(props) {
+        super()
+    }
+
+    render() {
+        return <h1>FUNCIONA</h1>
+    }
+}
+
+
+export default Heroes
