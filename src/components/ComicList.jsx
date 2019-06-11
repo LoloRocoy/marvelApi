94de99b08bef1428f9cad87c822199ad387a731b
ManeRomero <algoritmo717@gmail.com>
2019-06-11 18:00:02 +0200

    Comic controller routing well at Heroe Detail

--- src/components/ComicList.jsx
+++ src/components/ComicList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import style from '../styles/detail'
+import controller from '../controllers/comics'
 
 function ComicList (props) {
     return (
         <div>
-            <Link style={style.link} to={props.link} >{props.name}</Link>
+            <Link style={style.link} to={"/comics/" + controller.shorter(props.link)} >{props.name}</Link>
         </div>
     )
 }
--- src/controllers/comics.js
+++ src/controllers/comics.js
@@ -9,7 +9,14 @@ let comicByID = (id) => {
     return axios.get(config.urlBasis + config.comicByID + id + config.apiKey)
 }
 
+let shorter = (route) => {
+    if (typeof route === 'string') {
+        return route.slice(route.lastIndexOf('/') +1 )
+    }
+}
+
 export default {
     getComics,
-    comicByID
+    comicByID,
+    shorter
 }
